aeb101aeadee1fd9d185fe41a5603b14c20804eb
咖啡与茶 <79887029+SoftCreRevo@users.noreply.github.com>
2023-08-01 17:24:57 +0800

    Delete html directory

--- html/Ly/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "*****";
-$password = "*****";
-$dbname = "web";
-$connect=mysqli_connect($servername, $username, $password, $dbname);//连接数据库
-global $connect;
-?>
